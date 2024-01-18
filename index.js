2c26f98832bbd69a73241e3d170e0e39f42a3a8f
FahadShaik <shaikfahad904@gmail.com>
2024-01-18 11:01:04 +0530

    Validate city

--- index.js
+++ index.js
@@ -6,18 +6,43 @@ const searchInput = document.querySelector(".search-input");
 
 const getWeatherData = async function (city) {
   const getResponse = await fetch(API_URL + API_KEY + `&q=${city}`);
+  if (getResponse.status == 404) {
+    document.querySelector(".weather").style.display = "none";
+    document.querySelector(".error-message").style.display = "block";
+  }
   const data = await getResponse.json();
-  console.log(data);
 
   const cityName = document.querySelector(".city-name");
   const cityTemperature = document.querySelector(".temperature");
   const wind = document.querySelector(".wind-reading");
   const humidity = document.querySelector(".humidity-reading");
+  const weatherImg = document.querySelector(".weather-image");
+  const weatherContainer = document.querySelector(".weather");
+  const error = document.querySelector(".error-message");
 
   cityName.innerHTML = data.name;
   cityTemperature.innerHTML = data.main.temp + "°C";
   wind.innerHTML = data.wind.speed + " km/h";
   humidity.innerHTML = data.main.humidity + "°C";
+
+  if (data.weather[0].main === "Clear") {
+    weatherImg.src = "./images/clear.png";
+  } else if (data.weather[0].main === "Clouds") {
+    weatherImg.src = "./images/clouds.png";
+  } else if (data.weather[0].main === "Drizzle") {
+    weatherImg.src = "./images/drizzle.png";
+  } else if (data.weather[0].main === "Mist") {
+    weatherImg.src = "./images/mist.png";
+  } else if (data.weather[0].main === "Rain") {
+    weatherImg.src = "./images/rain.png";
+  } else if (data.weather[0].main === "Snow") {
+    weatherImg.src = "./images/snow.png";
+  }
+
+  if (data.cod === 200) {
+    weatherContainer.style.display = "block";
+    error.style.display = "none";
+  }
 };
 
 searchCity.addEventListener("click", () => {
